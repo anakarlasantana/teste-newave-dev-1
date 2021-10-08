bbd04f467d7a0bd297007611a05af2f8a2f89109
Henrique Artur <contato@henriqueartur.com>
2021-10-08 13:37:57 -0300

    :sparkles: Questão 2

---
+++ Q2_-_Power_Rangers.js
@@ -0,0 +1,12 @@
+/**
+ * Q.2 - Utilizando os arquivos presentes em power_ranges.zip
+ * faça uma QUERY que retorne as TOP 5 temporadas com a maior
+ * media de votos onde a nota da temporada tenha sido maior
+ * que '6.5'
+ * 
+ * Auxilio: você pode utilizar o site a seguir para carregar
+ * os dados e fazer as consultas
+ * @link https://sqliteonline.com
+ * 
+ * Dado retirado de https://www.kaggle.com/karetnikovn/power-rangers-dataset
+ */
