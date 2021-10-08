7d885dc17a50c863e1bce6970bf886c3b949e86c
Henrique Artur <contato@henriqueartur.com>
2021-10-08 13:38:33 -0300

    :sparkles: Questão 3

---
+++ Q3_-_MyAnimeList.js
@@ -0,0 +1,10 @@
+/**
+ * Desenvolva um endpoint em node.js + express, onde você
+ * deve passar como parâmetro o id de um anime do MyAnimeList
+ * e ele deve retornar: 'title', 'episodes', 'status', 'rating',
+ * 'score', 'rank', 'popularity' e 'synopsis'.
+ * 
+ * Para consumir os dados da MyAnimeList utilize a API abaixo ou
+ * qualquer outra que que desejar.
+ * @link https://jikan.docs.apiary.io/#
+ */
